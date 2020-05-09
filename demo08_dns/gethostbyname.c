edc1b1564c6caea38b5a1528f3395e93ce220aee
JamesWu9527 <38269730+JamesWu9527@users.noreply.github.com>
2020-05-09 10:54:27 +0800

    增加域名和地址获取主机信息的小例子

---
+++ demo08_dns/gethostbyname.c
@@ -0,0 +1,63 @@
+/*
+2020/5/9
+通过域名获取host信息
+使用函数 gethostbyname
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+void error_handling(char *message);
+
+int main()
+{
+	int i;
+	struct hostent *host;
+	char strDoName[20];
+	puts("请输入域名：");
+	gets(strDoName);
+
+	host = gethostbyname(strDoName);
+	if (!host)
+	{
+		error_handling("gethost.... error");
+	}
+
+	printf("Official name: %s \n", host->h_name);
+	for (i=0; host->h_aliases[i]; i++)
+	{
+		printf("Aliases %d %s \n", i+1, host->h_aliases[i]);
+	}
+
+	printf("Addrss type: %s \n", (host->h_addrtype == AF_INET? "IPV4":"IPV6"));
+
+	for (i=0; i<host->h_addr_list[i]; i++)
+	{
+		printf("IP addr %d %s \n", i+1, inet_ntoa(*(struct in_addr*)host->h_addr_list[i]));
+	}
+	
+	return 0;
+}
+
+void error_handling(char * message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+/*
+运行结果：
+jameswu@ubuntu:/mnt/hgfs/trunk/demo08_dns$ ./gethostbyname
+请输入域名：
+www.baidu.com
+Official name: www.a.shifen.com 
+Aliases 1 www.baidu.com 
+Addrss type: IPV4 
+IP addr 1 163.177.151.110 
+IP addr 2 163.177.151.109 
+*/
+
