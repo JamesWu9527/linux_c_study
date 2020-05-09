2759c6e65d3a76bc5025ebab017cf449d6a5fe18
JamesWu9527 <38269730+JamesWu9527@users.noreply.github.com>
2020-05-09 12:09:25 +0800

    增加socket缓冲区设置的相关代码示例

---
+++ demo09_sockInfo/get_buf.c
@@ -0,0 +1,49 @@
+/*
+2020/5/9
+获取socket缓冲区的大小
+根据关键字 SO_SNDBUF 和关键字 SO_RCVBUF
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+void error_handling(char *message);
+
+int main()
+{
+	int sock;
+	int snd_buf, rcv_buf, state;
+	socklen_t len;
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	len = sizeof(snd_buf);
+	state = getsockopt(sock, SOL_SOCKET, SO_SNDBUF, (void*)&snd_buf, &len);
+	if (state)
+	{
+		error_handling("getsockopt() error!");
+	}
+
+	len = sizeof(rcv_buf);
+	state = getsockopt(sock, SOL_SOCKET, SO_RCVBUF, (void*)&rcv_buf, &len);
+	if (state)
+	{
+		error_handling("getsockopt() error!");
+	}
+
+	printf("Input buffer size: %d \n", rcv_buf);
+	printf("Output buffer size: %d \n", snd_buf);
+	
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+
+
