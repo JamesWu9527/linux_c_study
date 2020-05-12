5deb605de8a127f07c80bf73f6590b3ba385af82
JamesWu9527 <38269730+JamesWu9527@users.noreply.github.com>
2020-05-12 09:49:06 +0800

    增加signal和sigaction函数的使用示例

---
+++ demo10_process/sigaction.c
@@ -0,0 +1,48 @@
+/*
+2020-5-12 09点31分
+一个关于 sigaction 的小例子
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void timeout(int sig);		//信号触发的函数
+void keycontrol(int sig);	
+
+int main()
+{
+	int i = 0;
+	struct sigaction act;
+	act.sa_handler = timeout;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGALRM, &act, 0);
+
+	alarm(2);
+
+	for (;i<3; i++)
+	{
+		puts("wait...");
+		sleep(100);
+	}
+	
+	return 0;
+}
+
+void timeout(int sig)
+{
+	if (SIGALRM == sig)
+	{
+		puts("Time out");
+	}
+	alarm(2);
+}
+
+void keycontrol(int sig)
+{
+	if (SIGINT == sig)
+	{
+		puts("CTRL + C pressed");
+	}
+}
---
+++ demo10_process/signal.c
@@ -0,0 +1,45 @@
+/*
+2020-5-11 14点55分
+一个关于signal的小例子
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void timeout(int sig);		//信号触发的函数
+void keycontrol(int sig);	
+
+int main()
+{
+	int i = 0;
+	signal(SIGALRM, timeout);
+	signal(SIGINT, keycontrol);
+	alarm(2);
+
+	for (;i<3; i++)
+	{
+		puts("wait...");
+		sleep(100);
+	}
+	
+	return 0;
+}
+
+void timeout(int sig)
+{
+	if (SIGALRM == sig)
+	{
+		puts("Time out");
+	}
+	alarm(2);
+}
+
+void keycontrol(int sig)
+{
+	if (SIGINT == sig)
+	{
+		puts("CTRL + C pressed");
+	}
+}
